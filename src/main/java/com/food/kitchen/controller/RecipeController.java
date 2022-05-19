0de41b8e532f5e38feadfae4138eacd63fe81fad
Lambert-Simnel <Josephsut@gmail.com>
2022-05-19 10:54:21 +0100

    Recipe controller initial pass

--- src/main/java/com/food/kitchen/controller/RecipeController.java
+++ src/main/java/com/food/kitchen/controller/RecipeController.java
@@ -1,4 +1,36 @@
 package com.food.kitchen.controller;
 
+import com.food.kitchen.model.Ingredient;
+import com.food.kitchen.model.Recipe;
+import com.food.kitchen.repository.IngredientRepository;
+import com.food.kitchen.repository.RecipeRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class RecipeController {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeController(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @GetMapping("/recipes")
+    public ResponseEntity<List<Recipe>> getAllRecipes() {
+        List<Recipe> recipes = recipeRepository.findAll();
+        return ResponseEntity.ok().body(recipes);
+    }
+
+    @PostMapping("/recipes")
+    public ResponseEntity<Recipe> createRecipe(@RequestBody Recipe recipe) {
+        Recipe result = recipeRepository.save(recipe);
+        return ResponseEntity.ok().body(result);
+    }
+
 }
