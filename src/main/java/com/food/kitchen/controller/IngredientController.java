63453219eef6e50512e1bb1ee992f6a36bc9b605
Lambert-Simnel <Josephsut@gmail.com>
2022-05-19 10:51:16 +0100

    Ingredient controller initial pass

--- src/main/java/com/food/kitchen/controller/IngredientController.java
+++ src/main/java/com/food/kitchen/controller/IngredientController.java
@@ -1,8 +1,34 @@
 package com.food.kitchen.controller;
 
+import com.food.kitchen.model.Ingredient;
+import com.food.kitchen.repository.IngredientRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class IngredientController {
+
+    private final IngredientRepository ingredientRepository;
+
+    public IngredientController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @GetMapping("/ingredients")
+    public ResponseEntity<List<Ingredient>> getAllIngredients() {
+        List<Ingredient> ingredients = ingredientRepository.findAll();
+        return ResponseEntity.ok().body(ingredients);
+    }
+
+    @PostMapping("/ingredients")
+    public ResponseEntity<Ingredient> createIngredient(@RequestBody Ingredient ingredient) {
+        Ingredient result = ingredientRepository.save(ingredient);
+        return ResponseEntity.ok().body(result);
+    }
 
 }
